7b372dc6637d3b0b9896892e74ccb5b6f5bb35f6
nicolas romano <nromanorumbo@gmail.com>
2021-08-25 22:30:32 -0300

    entrega 2 cambios del 25 de agosto

--- js/products.js
+++ js/products.js
@@ -38,11 +38,103 @@ function showCategoriesList(array){
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCTS_URL).then(function(resultObj){
         if (resultObj.status === "ok")
-        {
+        {                                       
             categoriesArray = resultObj.data;
+            //esto es para ordenar de forma ascendente si cambio el orden de a y b lo hago descendente
+            categoriesArray.sort(function(a, b){return a - b});
             //Muestro las categorías ordenadas
             showCategoriesList(categoriesArray);
         }
         
     });
 });
+//-----------------------------------Parte 2-----------------------------------------
+//aca hago la function para que filtre por minimo y maximo  
+let min;
+let max;
+
+function filtra(){
+    //hago un array para no sobrescribir el original
+    let preFiltro=[];
+    //capturo el valor de maximo y minimo del html
+    
+    let min= document.getElementById("pMin").value;
+    
+    let max=document.getElementById("pMax").value;
+
+    //lo que hago aca es ver si hay valores en los inputs del filtro
+    if(min==""){
+        min=0;
+    }else{
+        
+      min=validaQueSeaNumero(min); 
+    }
+    
+
+    if(max==""){
+            //no me corrio con maxint, preguntar por que???
+        max=1000000000;
+        
+    }else{
+        
+       max=validaQueSeaNumero(max);
+
+    }
+    
+    
+
+    //recorro el array y veo que objetos cumplen con los paramentros de filtrado
+    for (let i = 0; i<categoriesArray.length; i++) {
+
+        var productoActual = categoriesArray[i];
+
+            if (parseInt(productoActual.cost) > min && parseInt(productoActual.cost) < max) {
+
+                preFiltro.push(productoActual);
+            }
+      }
+      
+      //dibugo la tabla con los productos 
+      if(preFiltro.length>0){
+        showCategoriesList(preFiltro);  
+      }else{
+        document.getElementById("cat-list-container").innerHTML="NO HAY PRODUCTO QUE CUMPLAN CON ESOS PARAMETROS";
+      }
+      
+
+      //vacio los imputs
+      document.getElementById("pMin").value = "";
+      document.getElementById("pMax").value = "";
+}
+
+
+
+
+
+function validaQueSeaNumero(num){
+        //lo que hago aca es ver si hay valores en los inputs del filtro
+
+     if(!isNaN(num)){
+        alert("llegue");
+        num=parseInt(num);
+        alert(num);
+
+        return num;
+        }else {
+        alert("solo puede ingresar numeros o dejar el campo vacio");
+        }
+
+}
+
+const buscador=()=>{
+    let txtBuscado=document.querySelector("txtBuscador");
+
+    for(let producto of categoriesArray){
+
+        let elNombre=producto.name.toLowerCase();
+            //indexof retorna -1 cuando nno encuentra el valor, entonces si encontro el valor que retorna sera distinto de 1 
+        if(elNombre.indexof(txtBuscado) !== -1){
+
+        }
+    }
+}
