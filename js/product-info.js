42456bd51b5ac97c06ee7c45c33d8a19908a71b4
nromanorumbo <nromanorumbo@gmail.com>
2021-09-09 14:48:15 -0300

    coment

--- js/product-info.js
+++ js/product-info.js
@@ -2,7 +2,12 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 
-// traijo la consulta a la API
+// traijo la consulta a la API por el producto ese
+
+let comentariosProducto=[];
+let infoProductos={};
+let comentario={};
+
 
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
@@ -12,6 +17,22 @@ document.addEventListener("DOMContentLoaded", function(e){
 
             //Muestro las categorías ordenadas
             muestraProducto(infoProductos);
+        }
+        
+    });
+});
+
+
+//traigo de la api los comentarios de ese producto
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {                                       
+            comentariosProducto = resultObj.data;
+
+            //Muestro las categorías ordenadas
+            muestraComentarios(comentariosProducto);
         }
         
     });
@@ -59,9 +80,53 @@ function muestraProducto(producto){
 
 
 
-function caliProducto(cuantas){
+function caliProducto(valor){
 
+let coment=document.getElementById('comentarios').value;
+    if(coment.trim()!=""){
+
+           let usu=JSON.parse(sessionStorage.getItem("usuario"));
+            let fecha=new Date();
+
+            comentario.score=valor;
+            comentario.description=coment;
+            comentario.user=usu;
+            comentario.dateTime=fecha;
+
+            comentariosProducto.push(comentario);
+
+            muestraComentarios(comentariosProducto);
+
+    }
+    //no se si tengo que validar ya que el valor no lo puede ingresar el usuario, preguntar eso
+
+}
+
+
+
+
+//mostrar los comentarios
+
+
+function muestraComentarios(comentariosProducto){
+
+
+    let htmlContentToAppend = "";
     
+    for(let i = 0; i < comentariosProducto.length; i++){
+        let comentario = comentariosProducto[i];
+
+        htmlContentToAppend += `
+       <tr>
+            <td>${comentario.user}</td>
+            <td>${comentario.description}</td>
+            <td>${comentario.score}</td>
+       </tr>
+
+        `
+
+        document.getElementById("laTabla").innerHTML = (htmlContentToAppend);
 
 
+    }
 }
