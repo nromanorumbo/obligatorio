359f0bd8c008c3dec3d48586ff63f2506e0a0288
nicolas romano <nromanorumbo@gmail.com>
2021-10-14 22:58:10 -0300

    sin terminar

--- js/cart.js
+++ js/cart.js
@@ -17,17 +17,17 @@ document.addEventListener("DOMContentLoaded", function(e){
     });
 
 }); 
-   document.getElementById("cat-list-container").addEventListener("onchange",function(e){
-        getJSONData(CART_INFO_URL).then(function(resultObj){
-            if (resultObj.status === "ok")
-            {                                       
-                carritoArray = resultObj.data;
-              
-                //Muestro las categorías ordenadas
-                muestraCarrito(carritoArray);
-            }
-            
-        });
+
+//esto es para que este escuchando cuadno hay cambios
+   document.getElementById("canti").addEventListener("click",function(){
+
+        let subtotal=0;
+
+
+
+
+        document.getElementById("subtotal").innerHTML = (subtotal);
+
     });
 
 //lo que hago aca es con el array que traigo de la api armo el html para mostrarlo 
@@ -36,6 +36,8 @@ function muestraCarrito(array){
 
     let htmlContentToAppend = "";
     let total=0;
+    let cantidad=0;
+    let unitario=0;
 
     for(let i=0; i < array.articles.length; i++){
 
@@ -44,13 +46,15 @@ function muestraCarrito(array){
         let moneda=elCarro.currency;
         //transformo el texto a numero
         let cuanto=parseInt(elCarro.unitCost);
+        cantidad=elCarro.count;
 
+        unitario=cuanto;
 
         if (moneda=="UYU"){
-
-            cuanto=(cuanto/40)*elCarro.count;
+            unitario=(cuanto/40);
+            cuanto=unitario*cantidad;
             }else{
-               cuanto=cuanto*elCarro.count 
+               cuanto=unitario*cantidad; 
             }
             
              
@@ -62,7 +66,7 @@ function muestraCarrito(array){
 
 
         htmlContentToAppend += `
-        <div class="list-group-item list-group-item-action" onclick="cambio()">
+        <div class="list-group-item list-group-item-action" >
             <div class="row">
                 <div class="col-3">
                     <img src="` + elCarro.src + `" alt="` + elCarro.description + `" class="img-thumbnail">
@@ -70,14 +74,23 @@ function muestraCarrito(array){
                 <div class="col">
                     <div class=" justify-content-between">
                         <h4 class="mb-1">`+ elCarro.name +`</h4><br>
-                        <small class="text-muted"><strong>`+cuanto + `</strong> </small>
+                        <label for="cantidad">CANTIDAD : </label>
+                        <input type="number" id="canti"  min="1" max="100" value="`+cantidad+`"><br>
+
+                        <label for="unitario">UNITARIO : </label>
+                        <small class="text-muted" id="unitario"><strong>`+unitario + `</strong> </small><br>
+
+                        <label for="subTotal">SUBTOTAL : </label>
+                        <small class="text-muted" id="subtotal"><strong>`+cuanto + `</strong> </small>
+                        
+                        
                     </div>
 
                 </div>
             </div>
         </div>
         `
-
+          
         document.getElementById("cat-list-container").innerHTML = (htmlContentToAppend);
     }
     Swal.fire("TOTAL ACTUAL: U$S"+total.toString());
